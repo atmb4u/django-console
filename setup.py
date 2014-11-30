43e2b5d1fcf28b0f057c49c53440a96bf1e21bb9
Anoop Thomas Mathew <atmb4u@gmail.com>
2014-11-30 21:23:29 +0530

    setup file bumped version, long description

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name='django-console',
-    version='0.2',
+    version='0.3',
     author=u'Anoop Thomas Mathew',
     author_email='atmb4u@gmail.com',
     packages=['django-console'],
     url='http://github.com/atmb4u/django-console',
-    license='BSD Licence, see LICENCE.txt',
+    license='BSD Licence, see LICENSE',
     description='bash console in the browser for django admins',
-    long_description=open('README').read()
+    long_description=open('README.md').read()
 )
