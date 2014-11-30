1b2ce872ad50c31c343975a4b80e6f45cff0e8d4
Anoop Thomas Mathew <atmb4u@gmail.com>
2014-12-01 01:32:37 +0530

    works beautifully as an app

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
-# -*- coding: utf-8 -*-
-__author__ = 'atm'
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='django-console',
-    version='0.4',
-    author=u'Anoop Thomas Mathew',
-    author_email='atmb4u@gmail.com',
-    packages=['django-console'],
-    url='http://github.com/atmb4u/django-console',
-    license='BSD Licence, see LICENSE',
     description='bash console in the browser for django admins',
-    long_description=open('README.md').read()
+    keywords='django, bash',
+    packages=find_packages(),
+    include_package_data=True,
+    version="0.4.6",
+    author="Anoop Thomas Mathew",
+    author_email="atmb4u@gmail.com",
+    url='http://github.com/atmb4u/django-console',
+    classifiers=['Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Web Console',
+    'Topic :: Internet :: WWW/HTTP :: WSGI',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    license="BSD License",
+    platforms=["all"],
+    zip_safe=False
 )
